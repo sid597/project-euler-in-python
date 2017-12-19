ba736d2f9a3ce8baaa550fa9eb7cd2b265a14bd3
sid597 <sid597@sid597-To-be-filled-by-O-E-M>
2017-12-20 03:05:11 +0530

    Problem 4 solution

---
+++ problem_4.py
@@ -0,0 +1,9 @@
+l=[]
+for i in range(100,999):
+    for k in range(100,999):
+        n=i*k
+        if str(n) == str(n)[::-1]:
+            l.append(n)
+            print i,k,n
+
+print sorted (l),len (l)

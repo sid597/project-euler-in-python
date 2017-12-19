60aaf7a427b571e466e8b146b16201a0981aecef
sid597 <sid597@sid597-To-be-filled-by-O-E-M>
2017-12-20 02:47:59 +0530

    problem 3 solution

--- problem_3.py
+++ problem_3.py
@@ -1,1 +1,35 @@
-this is new file
+def sieve(primes_upto):
+    l = [True] * primes_upto
+    primes = []
+
+    for num in xrange(2, int(primes_upto ** 0.5 + 1.5)):
+         if l[num] :
+            primes.append(num)
+            for k in xrange(num, primes_upto, num):
+
+                l[ k] = False
+    for i in xrange(2, primes_upto):
+        if l[i] == True:
+            primes.append(i)
+    return primes
+
+
+l = sieve(int((60085147514 ** 0.5)))
+
+divisors = []
+
+for i in l:
+    if 600851475143 % i == 0:
+        divisors.append(i)
+
+print divisors
+print divisors[-1]
+
+
+
+
+
+
+
+
+

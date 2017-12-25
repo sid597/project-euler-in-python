350b2050701f9ec56351616ce6bcfc0e692d5856
Sid597 <siddharthdv77@gmail.com>
2017-12-26 02:18:41 +0530

    problem 14

--- problem_14.py
+++ problem_14.py
@@ -1,1 +1,57 @@
 "this is problem 14"
+n = 0
+
+
+def collatz(nu):
+    nm = nu
+    ctr = 0
+    while nm != 1:
+        if nm % 2 == 0:
+            nm /= 2
+            ctr += 1
+        else:
+            nm = nm * 3 + 1
+            ctr += 1
+    return ctr
+
+
+
+for i in range(2,1000000):
+    seq = collatz(i)
+    if seq > n:
+        n = seq
+        print i, seq
+
+print n, "this is the number"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
